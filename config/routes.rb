efbfb05ed64786326e31c20831fb4fb7266fd32a
Jason Yoon <jasony0206@gmail.com>
2016-10-09 20:57:10 -0700

    Add Users controller, add stub signup page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'landing_page#welcome'
+  get   '/signup',  to:  'users#new'
 end
