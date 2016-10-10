a703f103bdab06843f7e8a124c37b12823b621b8
Jason Yoon <jasony0206@gmail.com>
2016-10-10 14:24:46 -0700

    Remove old Users controller, clean up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
-  get 'users/new'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'landing_page#welcome'
-  get   '/signup',  to:  'users#new'
+
+  devise_for :users
 end
