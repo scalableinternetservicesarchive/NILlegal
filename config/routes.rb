ad5b5b53cde9e9592df5a37e15766eb069901b7e
Jason Yoon <jasony0206@gmail.com>
2016-10-11 23:57:48 -0700

    Customize devise to separate profile view from edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   root 'landing_page#welcome'
 
-  devise_for :users
+  devise_for :users,
+              controllers: {
+                registrations: 'users/registrations'
+              }
+
+  devise_scope :user do
+     get 'users/show', to: 'users/registrations#show', as: :show_user_registration
+  end
 end
