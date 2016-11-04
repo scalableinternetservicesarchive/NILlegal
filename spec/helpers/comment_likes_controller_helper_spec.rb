8111658f864efcc57cbcba5a9fba03aaec3f86da
Jason Yoon <jasony0206@gmail.com>
2016-11-04 09:35:24 -0700

    Fix naming issues that broke tests

--- spec/helpers/comment_likes_controller_helper_spec.rb
+++ spec/helpers/comment_likes_controller_helper_spec.rb
@@ -10,6 +10,6 @@ require 'rails_helper'
 #     end
 #   end
 # end
-RSpec.describe CommentLikeControllerHelper, type: :helper do
+RSpec.describe CommentLikesControllerHelper, type: :helper do
   pending "add some examples to (or delete) #{__FILE__}"
 end
