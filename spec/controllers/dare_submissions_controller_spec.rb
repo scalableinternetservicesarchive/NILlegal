2f090ff3f01309d9c08693bba8a37c31383608d5
Jordan <jpmemorize@g.ucla.edu>
2016-11-08 09:44:47 +0000

    Added some tests

--- spec/controllers/dare_submissions_controller_spec.rb
+++ spec/controllers/dare_submissions_controller_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe DareSubmissionsController, type: :controller do
           params: {dare_submission: {dare_id: dare.id}}
       end
   
-      it 'routes to dares#create' do
+      it 'routes to dares_submissions#create' do
         expect(:get => new_dare_submission_path).to route_to(
           :controller => "dare_submissions",
           :action => "new"
@@ -59,9 +59,6 @@ RSpec.describe DareSubmissionsController, type: :controller do
     context "valid user" do
       before do
         sign_in_user
-        
-        
-        
       end   
       
       context "with invalid attributes" do
@@ -111,20 +108,133 @@ RSpec.describe DareSubmissionsController, type: :controller do
       end
     end
     
-    # context "not logged in" do
-    #   it 'redirects to sign in' do
-    #     post :create,
-    #       params: {dare: FactoryGirl.attributes_for(:dare)}
-    #     expect(response).to redirect_to new_user_session_path
-    #   end 
-    #   it "post is not saved" do
-    #     expect{
-    #         post :create, 
-    #           params: {dare: FactoryGirl.attributes_for(:dare)}
-    #     }.to change(Dare,:count).by(0)
-    #   end
-    # end
+    context "not logged in" do
+      it 'redirects to sign in' do
+        post :create,
+          params: {dare: FactoryGirl.attributes_for(:dare)}
+        expect(response).to redirect_to new_user_session_path
+      end 
+      it "post is not saved" do
+        expect{
+            post :create, 
+              params: {dare: FactoryGirl.attributes_for(:dare)}
+        }.to change(Dare,:count).by(0)
+      end
+    end
   end
+  
+  
+  describe 'GET edit page' do
+    context "valid user" do
+      before do
+        sign_in_user
+        @dare = FactoryGirl.create(:dare)
+        post :create,
+          params: { dare_submission:{  
+                      content:"https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+                      description: "MyText",
+                      dare_id: @dare.id} }
+        get :edit,
+          params: {id: @dare.dare_submissions.first.id}
+      end
+      it 'routes to dares_submissions#edit' do
+        expect(:get => edit_dare_submission_path(@dare.dare_submissions.first.id)).to route_to(
+          :controller => "dare_submissions",
+          :action => "edit",
+          :id=>"#{@dare.dare_submissions.first.id}"
+        )
+      end
+  
+      it 'returns 200 OK' do
+        assert_response :success
+      end
+  
+      it 'renders correct view' do
+        assert_template :edit
+      end
+      
+      it "redirect to root_url when wrong dare submission" do
+        get :edit,
+              params: {id: 2}
+        expect(response).to redirect_to root_path
+      end
+    end
+    context "invalid user" do
+      it "redirect sign in when not logged in" do
+        get :edit,
+              params: {id: 1}
+        expect(response).to redirect_to new_user_session_path
+      end
+      
+    end
+  end
+  
+  describe 'PATCH dare_submission' do
+    context "valid user" do
+      before do
+        sign_in_user
+        @dare = FactoryGirl.create(:dare)
+        post :create,
+          params: { dare_submission:{  
+                      content:"https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+                      description: "MyText",
+                      dare_id: @dare.id} }
+      
+      end
+      context "invalid attributes" do
+        it 're-renders edit view' do
+          patch :update,
+            params: { 
+              id: @dare.dare_submissions.first.id,
+              dare_submission:{  
+                          content:"https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+                          description: "",
+                          dare_id: @dare.id}
+                          }
+          expect(response).to render_template :edit
+          assert_select "div.alert", "The form contains 1 error."
+        end
+      end
+      context "valid attributes" do
+        it 're-renders edit view' do
+          patch :update,
+            params: { 
+              id: @dare.dare_submissions.first.id,
+              dare_submission:{  
+                          content:"https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+                          description: "Test1",
+                          dare_id: @dare.id}
+                          }
+          expect(response).to redirect_to dare_path(@dare.id)
+          assert_equal flash.empty?, false
+          expect(flash[:success]).to match("Submission updated")
+        end
+        it "redirect to root_url when wrong dare submission" do
+          get :edit,
+            params: { 
+              id: 2,
+              dare_submission:{  
+                          content:"https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+                          description: "Test1",
+                          dare_id: @dare.id}
+                          }
+          expect(response).to redirect_to root_path
+        end
+      end
+      
+      
+      
+      
+      
+    end
+    
+    
+    
+    
+  end
+  
+  
+  
   
   
   
