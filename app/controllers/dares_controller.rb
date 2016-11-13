edce119c6f47c640e25590f68f72574ea8ca38d1
Jordan <jpmemorize@g.ucla.edu>
2016-11-13 12:30:32 +0000

    Added a search bar, assuming input is automatically sanitized through activerecord

--- app/controllers/dares_controller.rb
+++ app/controllers/dares_controller.rb
@@ -2,7 +2,11 @@ class DaresController < ApplicationController
   before_action :logged_in_user, only: [:new, :create]
 
   def index
-    @dares = Dare.all
+    if (params[:search])
+      @dares = Dare.where("title like ?", "%#{params[:search]}%")
+    else
+      @dares = Dare.all
+    end
   end
 
   def create
