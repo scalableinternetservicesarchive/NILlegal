77ad7e99ab243ffec36ae8c713cc2ef1648b184f
Jordan <jpmemorize@g.ucla.edu>
2016-11-23 04:29:14 +0000

    added extra text to hopefully properly creates winning_submission column

--- db/migrate/20161115021502_add_dare_winner_id_to_dare.rb
+++ db/migrate/20161115021502_add_dare_winner_id_to_dare.rb
@@ -1,6 +1,6 @@
 class AddDareWinnerIdToDare < ActiveRecord::Migration[5.0]
   def change
-    add_reference :dares, :dare_winner
-    add_foreign_key :dares, :dare_submissions, column: :dare_winner
+    add_reference :dares, :winning_submission, references: :dare_submissions, index: true
+    add_foreign_key :dares, :dare_submissions, column: :winning_submission
   end
 end
