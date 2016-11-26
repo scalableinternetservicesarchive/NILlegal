667ee548997e6e90a64522baaf07c8110e4d6860
Jordan <jpmemorize@g.ucla.edu>
2016-11-26 12:20:16 +0000

    Added some server caching for dares

--- app/helpers/dares_helper.rb
+++ app/helpers/dares_helper.rb
@@ -1,2 +1,11 @@
 module DaresHelper
+  def cache_key_for_dare(dare)
+    "dare-#{dare.id}-#{dare.updated_at}-#{dare.comments.count}-#{dare.dare_submissions.count}"
+  end
+  
+  def cache_key_for_dare_table
+    "dare-table-#{Dare.maximum(:updated_at)}-#{Comment.maximum(:updated_at)}-#{DareSubmission.maximum(:updated_at)}"
+  end
+
+  
 end
