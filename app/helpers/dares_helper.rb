34794ef2b515dd3d9ca5c722e957305515e347fb
Jordan <jpmemorize@g.ucla.edu>
2016-11-28 06:21:51 +0000

    Went a little crazy with caching for dares due to a few dynamic portions

--- app/helpers/dares_helper.rb
+++ app/helpers/dares_helper.rb
@@ -1,6 +1,22 @@
 module DaresHelper
   def cache_key_for_dare(dare)
-    "dare-#{dare.id}-#{dare.user.name}-#{dare.updated_at}-#{dare.comments.count}-#{dare.dare_submissions.count}"
+    "dare-#{dare.id}-#{dare.user.name}-#{dare.updated_at}-#{dare.comments.count}-#{dare.dare_submissions.count}-#{time_ago_in_words(dare.created_at)}"
+  end
+  
+  def cache_key_for_dare_time(dare)
+    "dare_time-#{dare.id}-#{time_ago_in_words(dare.created_at)}"
+  end
+  
+  def cache_key_for_dare_update(dare)
+    "dare_update-#{dare.id}-#{dare.updated_at}"
+  end
+  
+  def cache_key_for_dare_user(dare)
+    "dare_user-#{dare.id}-#{dare.user.name}"
+  end
+  
+  def cache_key_for_dare_count(dare)
+    "dare_count-#{dare.id}-#{dare.comments.count}-#{dare.dare_submissions.count}"
   end
   
   def cache_key_for_dare_table
